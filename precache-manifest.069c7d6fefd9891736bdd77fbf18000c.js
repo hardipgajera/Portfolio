2e2b1acb456ac50843388189078391969f6b60ef
AbhishekGajera <abhishekgajera3140@gmail.com>
2022-01-09 18:25:06 +0530

    Updates

---
+++ precache-manifest.069c7d6fefd9891736bdd77fbf18000c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08f94296a4e4c5415e25eb9e255acb9",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dc5e5ced6ecf4d2e0700",
+    "url": "/Portfolio/static/css/2.f66b8083.chunk.css"
+  },
+  {
+    "revision": "dc6c9b90f584a5813022",
+    "url": "/Portfolio/static/css/main.9eeab255.chunk.css"
+  },
+  {
+    "revision": "dc5e5ced6ecf4d2e0700",
+    "url": "/Portfolio/static/js/2.f3919a7b.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Portfolio/static/js/2.f3919a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6c9b90f584a5813022",
+    "url": "/Portfolio/static/js/main.c22cd3d0.chunk.js"
+  },
+  {
+    "revision": "59bba9ec99bac52f31af",
+    "url": "/Portfolio/static/js/runtime-main.1a8ab931.js"
+  },
+  {
+    "revision": "b144d5719890dc50fbe7556e28e05e9a",
+    "url": "/Portfolio/static/media/Hardip.b144d571.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
